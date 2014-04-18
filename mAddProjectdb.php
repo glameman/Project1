c1bcad9605c0a07e518c0ea1d3d5d7048ed76f10
HIGHonLIFE1012 <highonlife1012@yahoo.com>
2014-04-18 16:38:32 -0700

    Projects Database (04/18/2014)

---
+++ mAddProjectdb.php
@@ -0,0 +1,33 @@
+<?php
+	
+	ob_start();
+
+	session_start();
+
+	$host = "localhost";
+	$username = "root";
+	$password = "pass";
+	$database =  "my_db";
+	$table = "project";
+
+
+	$conn = mysql_connect("localhost", "root", "pass") or die(mysql_error());
+	echo "connected<br><br>";
+
+	mysql_select_db("$database");
+	echo "database found<br><br>";
+
+	// escape variables for security
+	$projname = $_POST['projname'];
+	$status = $_POST['status'];
+	$startdate = $_POST['startdate'];
+	$enddate = $_POST['enddate'];
+	$managerID = $_SESSION['UID'];
+
+	mysql_query("INSERT INTO $table(ProjectName, MID, Status, StartDate, ExpectedEndDate) VALUES ('$projname', '$managerID', '$status', '$startdate', '$enddate')") or die(mysql_error());
+
+	mysql_close($conn);
+
+	$msg = "Project Successfully Added";
+	header("Location:Manager.php?msg=$msg");
+?>
