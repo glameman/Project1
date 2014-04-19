a48af5b8c606fe6f17fcf2d4d0ef41af29b988b0
HIGHonLIFE1012 <highonlife1012@yahoo.com>
2014-04-19 16:11:38 -0700

    Updated mAddRequirements (04/19/2014 | 4:11 PM)

--- mAddRequirements.php
+++ mAddRequirements.php
@@ -1,3 +1,9 @@
+<?php
+
+session_start();
+
+?>
+
 <!DOCTYPE html>
 
 <html>
@@ -12,55 +18,70 @@
         <link href="css/simple-sidebar.css" rel="stylesheet">
 
     </head>
-  <body bgcolor="#DCDCDC">
-    <header>
-        <nav class="navbar navbar-default" role="navigation">
-          <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="index.php">TAG Project Management Tool</a>
+    <body style="background-color:#F5F5DC;">
+        <!-- Sidebar -->
+            <div id="sidebar-wrapper">
+                <ul class="sidebar-nav">
+                    <li class="sidebar-brand"><a href="manager.php"><font color="white">Welcome, <?php echo $_SESSION['FirstName']; ?></font></a>
+                    </li>
+                    <?php
+                    if($_SESSION['MViewProj'] == 1)
+                        echo "<li><a href='mViewProjects.php'>View Projects</a></li>";
+                    ?>
+                    <?php
+                    if($_SESSION['MAddProj'] == 1)
+                        echo "<li><a href='mAddProject.php'>Add Project</a></li>";
+                    ?>
+                    <?php
+                    if($_SESSION['MAddProj'] == 1)
+                        echo "<li><a href='mAddRequirements.php'>Add Project Requirement</a></li>";
+                    ?>
+                    <li><a href="logout.php"><div class="glyphicon glyphicon-off"></div> Sign Out</a>
+                    </li>
+                </ul>
             </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                  <ul class="nav navbar-nav navbar-right">
-                    <li><a href="manager.php">Home Page</a></li>
-                  </ul>
-                </div><!-- /.navbar-collapse -->
-          </div><!-- /.container-fluid -->
-        </nav>
-    </header>
-    <H1 ALIGN="CENTER">Create New Requirement</H1>
-    <HR WIDTH="36%" SIZE="5" NOSHADE="NOSHADE">
-        <form tag="Create Logon" action="mAddRequirementsdb.php" method="post">
-            <div align="center">
-                <?php if(isset($_GET['msg']))
-                    echo "<font color='red'><br>" . $_GET['msg'] . "</font>";
-                ?>
-                Project ID: <input type="int" name="PID"/>
-                </br></br>
-                Requirement Description*: <input type="text" name="reqdescription" />
-                </br></br>
-                Type*: <select class="selectpicker" name="type">
-                    <option>Manager</option>
-                    <option>Worker</option>
-                </select>
-                </br><br>
-                Time Required*: <input type="text" name="timerequired" />
-                </br><br>
-                    <input type="submit" value="Submit" />
-                    </br><br>
-                <p>Note: Please make sure your details are correct before submitting and that all fields marked with * are completed!.</p>
-            </div>
-        </form>
- 
+        <div id="wrapper">
+            <div class="page-content inset">
 
-    
+                <H1 ALIGN="CENTER">Create New Requirement</H1>
+                <HR WIDTH="36%" SIZE="5" NOSHADE="NOSHADE">
+                    <form tag="Create Logon" action="mAddRequirementsdb.php" method="post">
+                        <div align="center">
+                            <?php if(isset($_GET['msg']))
+                                echo "<font color='red'><br>" . $_GET['msg'] . "</font>";
+                            ?>
+                            Project ID*: <input type="int" name="PID"/>
+                            </br></br>
+                            Type*: <input type = "text" name="type"/>
+                            </br><br>
+                            Time Required (hrs)*: <input type="text" name="timerequired" />
+                            </br><br>
+                            Requirement Description*: <input type="text" name="reqdescription"/>
+                            </br></br>
+                            Assign Worker:
+                            <select name="worker"><option value=""> --Select Category-- </option>
+                                <?php
+                                    $con = mysql_connect("localhost","root","pass") or die("Could not connect to database");
+                                    mysql_select_db("my_db") or die("Could not find database");
+                                    
+                                    $tenantID = $_SESSION['TID'];
+                                    $query= mysql_query("SELECT UID, FirstName, LastName FROM users WHERE TID = $tenantID AND Type = 'Worker'");
+
+                                    while($row = mysql_fetch_assoc($query))
+                                    {
+                                        echo "<option value='" . $row['UID'] . "'>" . $row['FirstName'] . " " . $row['LastName'] . "</OPTION>";
+                                    }
+                                    mysql_close();
+                                ?>
+                            </select>
+                            </br></br>
+                            <input type="submit" value="Submit" />
+                            </br></br>
+                            <p>Note: Please make sure your details are correct before submitting and that all fields marked with * are completed!.</p>
+                        </div>
+                    </form>
+            </div>
+        </div>   
  
         <script src="js/bootstrap.min.js"></script>
   </body>
