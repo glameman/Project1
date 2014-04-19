e5e79aa934a10a0e93f84f151716d3bfb42f2177
HIGHonLIFE1012 <highonlife1012@yahoo.com>
2014-04-18 20:28:19 -0700

    Add Requirements Database (04/18/2014 | 8:28 PM)

---
+++ mAddRequirementsdb.php
@@ -0,0 +1,34 @@
+<?php
+	
+	ob_start();
+
+	session_start();
+
+	$host = "localhost";
+	$username = "root";
+	$password = "pass";
+	$database =  "my_db";
+	$table = "requirements";
+
+
+	$conn = mysql_connect("localhost", "root", "pass") or die(mysql_error());
+	echo "connected<br><br>";
+
+	mysql_select_db("$database");
+	echo "database found<br><br>";
+
+	// escape variables for security
+	$reqdescription = $_POST['reqdescription'];
+	$type = $_POST['type'];
+	$timerequired = $_POST['timerequired'];
+	$projectID = $_POST['PID'];
+	$managerID = $_SESSION['UID'];
+	$status = "Started";
+
+	mysql_query("INSERT INTO $table(PID, Req_Description, MID, Type, TimeRequired, Status) VALUES ('$projectID', '$reqdescription', '$managerID', '$type', '$timerequired', '$status')") or die(mysql_error());
+
+	mysql_close($conn);
+
+	$msg = "Requirement Successfully Added";
+	header("Location:Manager.php?msg=$msg");
+?>
