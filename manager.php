aa80dee2e7e332ea7c18054a4d382f180af1c2d8
HIGHonLIFE1012 <highonlife1012@yahoo.com>
2014-04-28 10:47:33 -0700

    Updated manager.php (4/28/2014 | 10:47 AM)

--- manager.php
+++ manager.php
@@ -44,28 +44,41 @@ session_start();
 	            </ul>
 	        </div>
 			<div class="page-content inset">
-				<h1 align="center">MANAGER PAGE
+				<h1 ALIGN="CENTER">Manager Information</h1>
+				<HR WIDTH="36%" SIZE="10" NOSHADE="NOSHADE">
+				<h2 align="center">
 
 					<?php
 
+					$con = mysql_connect("localhost","root","pass") or die("Could not connect to database");
+						mysql_select_db("my_db") or die("Could not find database");
+					
+					$tenantID = $_SESSION['TID'];
+					$query = mysql_query("SELECT CompanyLogoName 
+										FROM tenant 
+										WHERE TID = $tenantID");
+
 					echo "<p>";
-					echo "<p>";
-					echo "User ID: " . $_SESSION['UID'];
 					echo "<p>";
 					echo "First Name: " . $_SESSION['FirstName'];
 					echo "<p>";
 					echo "Last Name: " . $_SESSION['LastName'];
 					echo "<p>";
-					echo "Type: " . $_SESSION['Type'];
-					echo "<p>";
-					echo "TID: " . $_SESSION['TID'];
-					echo "<p>";
 
+					while($row = mysql_fetch_assoc($query)){
+					echo "Company: " . $row['CompanyLogoName'];
+					echo "<p>";
+					}
+
+					mysql_close($con);
+
+					echo "User ID: " . $_SESSION['UID'];
+					echo "<p>";
 
 					?>
 
 
-				</h1>
+				</h2>
 			</div>
 		</div>
 		
