7185f23b652c2564fbeebea19d41815456de8277
HIGHonLIFE1012 <highonlife1012@yahoo.com>
2014-04-28 10:49:03 -0700

    Update worker.php

--- worker.php
+++ worker.php
@@ -42,26 +42,41 @@ if($_SESSION['Type'] != 'Worker')
 	        </div>
 
 			<div class="page-content inset">
-				<h1 align="center">WORKER PAGE
+				<h1 ALIGN="CENTER">Worker Information</h1>
+				<HR WIDTH="36%" SIZE="10" NOSHADE="NOSHADE">
+				<h2 align="center">
 
 					<?php
 
+					$con = mysql_connect("localhost","root","pass") or die("Could not connect to database");
+						mysql_select_db("my_db") or die("Could not find database");
+					
+					$tenantID = $_SESSION['TID'];
+					$query = mysql_query("SELECT CompanyLogoName 
+										FROM tenant 
+										WHERE TID = $tenantID");
+
 					echo "<p>";
-					echo "<p>";
-					echo "User ID: " . $_SESSION['UID'];
 					echo "<p>";
 					echo "First Name: " . $_SESSION['FirstName'];
 					echo "<p>";
 					echo "Last Name: " . $_SESSION['LastName'];
 					echo "<p>";
-					echo "Type: " . $_SESSION['Type'];
+
+					while($row = mysql_fetch_assoc($query)){
+					echo "Company: " . $row['CompanyLogoName'];
 					echo "<p>";
-					echo "TID: " . $_SESSION['TID'];
+					}
+
+					mysql_close($con);
+
+					echo "User ID: " . $_SESSION['UID'];
+					echo "<p>";
 
 					?>
 
 
-				</h1>
+				</h2>
 			</div>
 		</div>
 		
