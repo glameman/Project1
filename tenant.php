f1628265b777eddca78e3db248e5a6ee70282ea0
HIGHonLIFE1012 <highonlife1012@yahoo.com>
2014-04-28 10:48:23 -0700

    Updated tenant.php (4/28/2014 | 10:48 AM)

--- tenant.php
+++ tenant.php
@@ -50,25 +50,41 @@ if($_SESSION['Type'] != 'Tenant')
 	        </div>
 		
 			<div class="page-content inset">
-				<h1 align="center">TENANT PAGE
+				<h1 ALIGN="CENTER">Tenant Information</h1>
+				<HR WIDTH="36%" SIZE="10" NOSHADE="NOSHADE">
+				<h2 align="center">
 
 					<?php
 
+					$con = mysql_connect("localhost","root","pass") or die("Could not connect to database");
+						mysql_select_db("my_db") or die("Could not find database");
+					
+					$tenantID = $_SESSION['TID'];
+					$query = mysql_query("SELECT CompanyLogoName 
+										FROM tenant 
+										WHERE TID = $tenantID");
+
 					echo "<p>";
-					echo "<p>";
-					echo "Tenant ID: " . $_SESSION['TID'];
 					echo "<p>";
 					echo "First Name: " . $_SESSION['FirstName'];
 					echo "<p>";
 					echo "Last Name: " . $_SESSION['LastName'];
 					echo "<p>";
-					echo "Type: " . $_SESSION['Type'];
+
+					while($row = mysql_fetch_assoc($query)){
+					echo "Company: " . $row['CompanyLogoName'];
+					echo "<p>";
+					}
+
+					mysql_close($con);
+
+					echo "Tenant ID: " . $_SESSION['TID'];
 					echo "<p>";
 
 					?>
 
 
-				</h1>
+				</h2>
 			</div>
 		</div>
 
